efd074d93f785fa193b828156b6d7466cb008ec3
Eva Pace <eba.pachi@gmail.com>
2024-06-28 18:43:53 -0300

    add competitors cache

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,7 @@ fn build_client() -> reqwest::Client {
         .unwrap()
 }
 
-async fn get_competitor_stats(
+async fn fetch_competitor_stats(
     client: &reqwest::Client,
     id: &str,
 ) -> Result<CompetitorStats, Box<dyn std::error::Error>> {
@@ -74,7 +74,7 @@ async fn get_competitor_stats(
         .await?)
 }
 
-async fn get_competitors(
+async fn fetch_competitors(
     client: &reqwest::Client,
 ) -> Result<SeasonCompetitors, Box<dyn std::error::Error>> {
     let api_key = env::var("SPORTRADAR_API_KEY").expect("SPORTRADAR_API_KEY env var is not set");
@@ -91,21 +91,89 @@ async fn get_competitors(
         .await?)
 }
 
+use std::fs;
+use std::path::PathBuf;
+
+struct Cache {
+    base_path: PathBuf,
+    competitors: Option<SeasonCompetitors>,
+}
+
+impl Cache {
+    fn new() -> Self {
+        // ideally could use lib to run consistently
+        // on windows
+        #[allow(deprecated)]
+        let mut base_dir = std::env::home_dir().expect("should have home dir");
+        base_dir.push(".matchday");
+        // we ignore the error if it already exists
+        let _ = fs::create_dir(&base_dir);
+
+        let mut competitors_file = base_dir.clone();
+        competitors_file.push("competitors.json");
+
+        let competitors = if competitors_file.exists() {
+            let raw_competitors = fs::read_to_string(competitors_file).unwrap();
+            Some(
+                serde_json::from_str(&raw_competitors)
+                    .expect("competitors cache should be valid JSON"),
+            )
+        } else {
+            None
+        };
+
+        Self {
+            base_path: base_dir,
+            competitors,
+        }
+    }
+
+    /// gets from loaded cache or fetches them and saves to cache
+    async fn get_competitors(
+        &mut self,
+        client: &reqwest::Client,
+    ) -> Result<&SeasonCompetitors, Box<dyn std::error::Error>> {
+        match self.competitors {
+            Some(ref competitors) => Ok(competitors),
+            None => {
+                let retry_strategy = ExponentialBackoff::from_millis(500).map(jitter).take(3);
+                let competitors =
+                    Retry::spawn(retry_strategy, || fetch_competitors(client)).await?;
+
+                let mut competitors_file = self.base_path.clone();
+                competitors_file.push("competitors.json");
+
+                let _ = fs::File::create(&competitors_file);
+                fs::write(
+                    &competitors_file,
+                    serde_json::to_string(&competitors)
+                        .expect("competitors should be serializable to JSON"),
+                )?;
+
+                self.competitors = Some(competitors);
+
+                Ok(self.competitors.as_ref().unwrap())
+            }
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
-    let client = build_client();
+    let mut cache = Cache::new();
 
+    let client = build_client();
     let retry_strategy = ExponentialBackoff::from_millis(500).map(jitter).take(3);
 
-    let competitors = Retry::spawn(retry_strategy.clone(), || get_competitors(&client)).await?;
+    let competitors = cache.get_competitors(&client).await?;
     // println!("{competitors:#?}");
 
-    for competitor in competitors.season_competitors {
+    for competitor in &competitors.season_competitors {
         println!("fetching for id: {}", competitor.id);
         let competitor_stats = Retry::spawn(retry_strategy.clone(), || {
-            get_competitor_stats(&client, &competitor.id)
+            fetch_competitor_stats(&client, &competitor.id)
         })
         .await?;
 
