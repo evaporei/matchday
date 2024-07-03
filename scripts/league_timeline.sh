f553c9219b18b780b30a58dc15b4890723677635
Eva Pace <eba.pachi@gmail.com>
2024-07-03 18:57:06 -0300

    remove unused script folder and examples

--- scripts/league_timeline.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-API_KEY=$1
-
-curl --request GET \
-     --url "https://api.sportradar.com/soccer/trial/v4/en/sport_events/sr%3Asport_event%3A41762859/league_timeline.json?api_key=${API_KEY}" \
-     --header 'accept: application/json'
--- scripts/season_competitor_statistics.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-API_KEY=$1
-
-curl --request GET \
-     --url "https://api.sportradar.com/soccer/trial/v4/en/seasons/sr%3Aseason%3A93741/competitors/sr%3Acompetitor%3A17/statistics.json?api_key=${API_KEY}" \
-     --header 'accept: application/json'
